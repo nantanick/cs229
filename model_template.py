4e00ce5cf0a73ed845c93a79954b5f33eeddeb3e
Methin <methinp@stanford.edu>
2019-05-24 21:51:01 -0700

    update model_template

---
+++ model_template.py
@@ -0,0 +1,19 @@
+# Abstract Class for model
+
+class AlphaModel():
+    def __init__(self):
+        raise NotImplementedError
+
+    def train(self, X, Y):
+        # store model in self.model
+        # each row of X is one feature vector
+        # Y is a vector of labels
+        raise NotImplementedError
+
+    def predict(self, X):
+        # each row of X is one feature vector
+        # output is a vector of returns
+        raise NotImplementedError
+
+    def getModelInfo():
+        raise NotImplementedError
