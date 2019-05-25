b161d2422bdd20af132c2c8c2904aa85f1a8ce06
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2019-05-24 20:31:20 -0700

    add data loader

---
+++ util.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import glob
+
+def get_data():
+    data = []
+    for f in glob.glob('./data/*.json'):
+        temp = pd.read_json(f)
+        data.append(temp)
+    return pd.concat(data)
+
+
+if __name__ == '__main__':
+    print(get_data().head())
