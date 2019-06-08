6e42b2e534b6041b39fcee8417af81e5b079994b
Methin <methinp@stanford.edu>
2019-06-08 17:21:27 +0700

    for poster

--- plots_for_poster.py
+++ plots_for_poster.py
@@ -5,6 +5,7 @@ import os
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, f1_score, accuracy_score
 
 TRANSACTION_COST = 0.109 #10.9 bps according to http://www.integrity-research.com/equity-commission-rates-remain-steady/
 
@@ -35,15 +36,16 @@ Portfolio Generation
 '''
 result = dict()
 for key, pred in prediction_dict.items():
-    portfolio = eval.portfolio_generator(principal = 1000.,prediction = pred, true_price = price,threshold = [0.499,0.501] ,leverage = 1 ,short = True, transc = 0.)
+    portfolio = eval.portfolio_generator(principal = 1000.,prediction = pred, true_price = price,threshold = [0.45,0.55] ,leverage = 1 ,short = True, transc = 0.0015)
     abs_profit, profit, sharpe, profit_per_hr = eval.profit_eval(portfolio)
     result[str(key)] = portfolio
     print(portfolio)
+    print(key + 'AUC Score', roc_auc_score(TRUE_CLASS, pred))
     print(key +'Profit/hr', profit_per_hr)
-    print(key +'sharpe', sharpe)
+    #print(key +'sharpe', sharpe)
     print(key +'annaul profit', profit)
     print(key + "Accuaracy", eval.accuracy(prediction = pred,true_class = TRUE_CLASS))
-    print(key + "F1Score" , eval.f1score(prediction = pred,true_class = TRUE_CLASS, average='macro'))
+    #print(key + "F1Score" , eval.f1score(prediction = pred,true_class = TRUE_CLASS, average='macro'))
     print('*'*50)
 
 #Plot
